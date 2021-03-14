7c113472a633fa047f8d238aaf60dd94b840214c
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2021-03-14 15:10:34 -0400

    change code

--- model/Executor.py
+++ model/Executor.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as functional
 import torch.optim as optim
+from tqdm.notebook import tqdm
 import numpy as np
 
 
@@ -13,7 +14,7 @@ class Executor:
         self.gd_reducer = optim.SGD(self.network.parameters(), lr=0.01, momentum=0.9)
 
     def load_dataset(self):
-        # set the dataset
+        # set the dataset part of preprocessing
         # self.train_data = None
         pass
 
@@ -50,22 +51,26 @@ class Executor:
         calc_min_loss = float("inf")
         all_accuracy_vals = []
         # Track loading using tqdm
-        for iter in tqdm(range(0, num_iters), total=num_iters, desc="train model"):
+        for _ in tqdm(range(0, num_iters), total=num_iters, desc="train model"):
             self.network.train()
             for item in data_src:
+                # 0:images 1:labels
                 result = self.network(item[0])
-                calc_loss = self.entropy_loss(result, item[1]).item()
-                all_loss_vals.append(calc_loss)
+                calc_loss = self.entropy_loss(result, item[1])
+                all_loss_vals.append(calc_loss.item())
                 self.gd_reducer.zero_grad()
                 calc_loss.backward()
                 self.gd_reducer.step()
-            total_iter_loss = sum(each_iter_loss)
-            covered_so_far = len(each_iter_loss)
+            total_iter_loss = sum(all_loss_vals)
+            covered_so_far = len(all_loss_vals)
             avg_loss = total_iter_loss / covered_so_far
-            each_iter_loss.append(each_iter_loss)
+            each_iter_loss.append(avg_loss)
             calc_accuracy = self.calculate_model_accuracy(data_src)
             all_accuracy_vals.append(calc_accuracy)
+            print("current iter acc:", calc_accuracy)
+            print("current iter loss:", calc_min_loss)
             calc_min_loss = min(avg_loss, calc_min_loss)
+
         return all_loss_vals, each_iter_loss, all_accuracy_vals
 
 # if __name__ == '__main__':
