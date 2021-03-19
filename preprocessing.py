b5d5c49783ee5a0fdc81ada2fcea747cf20436da
zapouria <spzahraei@yahoo.com>
2021-03-19 12:23:48 -0400

    preprocessing fixed.

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,9 @@
+import torch
 import torchvision
 from torchvision.datasets import ImageFolder
 import sklearn
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 
 def load_data(path):
     reshape_size = torchvision.transforms.Resize((32,32))
@@ -11,8 +13,8 @@ def load_data(path):
 
 def split_data(dataset):
     # The training data is 60% of the full data set. The validation and testing data each comprise 20% of the original data.
-    train_d, test_d = sklearn.model_selection.train_test_split(dataset, test_size=0.2, random_state=30)
-    train_d, validation_d = sklearn.model_selection.train_test_split(train_d, test_size=0.25, random_state=30)
+    train_d, test_d = train_test_split(dataset, test_size=0.2, random_state=30)
+    train_d, validation_d = train_test_split(train_d, test_size=0.25, random_state=30)
     return train_d, validation_d, test_d
 
 def train_dataloarder(dataset):
